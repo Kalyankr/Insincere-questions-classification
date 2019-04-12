ce3e103b36e1944b86970ff9add08c6a84e4c40a
kalyan reddy <Kalyan.katla@gmail.com>
2019-04-12 13:57:43 -0700

    Create train.py

---
+++ train.py
@@ -0,0 +1,14 @@
+
+# function for model training and prediction.
+
+def train_pred(model, train_X, train_y, val_X, val_y, epochs=2, callback=None):
+    for e in range(epochs):
+        model.fit(train_X, train_y, batch_size=512, epochs=1, validation_data=(val_X, val_y), callbacks = callback, verbose=0)
+        pred_val_y = model.predict([val_X], batch_size=1024, verbose=0)
+
+        best_score = metrics.f1_score(val_y, (pred_val_y > 0.33).astype(int))
+        print("Epoch: ", e, "-    Val F1 Score: {:.4f}".format(best_score))
+
+    pred_test_y = model.predict([test_X], batch_size=1024, verbose=0)
+    print('=' * 60)
+    return pred_val_y, pred_test_y, best_score
